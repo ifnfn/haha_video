c0d8e1927640a8274cdcfbe073e88dcedd438706
zhuzhg <zhuzhg@nationalchip.com>
2013-12-26 20:38:35 +0800

    Initial Commit

---
+++ kolatv/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  kolatv
+//
+//  Created by Silicon on 13-12-26.
+//  Copyright (c) 2013年 Silicon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

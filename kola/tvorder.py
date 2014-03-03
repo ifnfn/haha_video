257ab64837265aed7d2b5d82cba426b1fab61512
zhuzhg <zhuzhg@nationalchip.com>
2014-03-03 13:49:37 +0800

    增加直播节目序号

---
+++ kola/tvorder.py
@@ -0,0 +1,79 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from .utils import GetPinYin
+TVOrder = [
+    "CCTV-1 综合",
+    "CCTV-2 财经",
+    "CCTV-3 综艺",
+    "CCTV-4 (亚洲)",
+    "CCTV-4 (欧洲)",
+    "CCTV-4 (美洲)",
+    "CCTV-5 体育",
+    "CCTV-6 电影",
+    "CCTV-7 军事农业",
+    "CCTV-8 电视剧",
+    "CCTV-9 纪录",
+    "CCTV-9 纪录(英)",
+    "CCTV-10 科教",
+    "CCTV-11 戏曲",
+    "CCTV-12 社会与法",
+    "CCTV-13 新闻",
+    "CCTV-14 少儿",
+    "CCTV-15 音乐",
+    "CCTV-NEWS",
+    "CCTV-Français",
+    "CCTV-Español",
+    "CCTV-العربية",
+    "CCTV-Русский",
+    "CCTV体育赛事",
+    "安徽卫视",
+    "北京卫视",
+    "重庆卫视",
+    "东方卫视",
+    "东南卫视",
+    "广东卫视",
+    "广西卫视",
+    "甘肃卫视",
+    "贵州卫视",
+    "河北卫视",
+    "河南卫视",
+    "黑龙江卫视",
+    "湖北卫视",
+    "吉林卫视",
+    "江西卫视",
+    "康巴卫视",
+    "辽宁卫视",
+    "旅游卫视",
+    "内蒙古卫视",
+    "宁夏卫视",
+    "青海卫视",
+    "山东卫视",
+    "山东教育台",
+    "深圳卫视",
+    "陕西卫视",
+    "山西卫视",
+    "四川卫视",
+    "天津卫视",
+    "西藏卫视",
+    "厦门卫视",
+    "新疆卫视",
+    "香港卫视",
+    "延边卫视",
+    "云南卫视",
+    "浙江卫视",
+]
+
+def GetOrder(name):
+    i = 0
+    for n in TVOrder:
+        if n == name:
+            return '%04d' % i
+        
+        i += 1
+    pinyin = GetPinYin(name)
+    if pinyin:
+        return '9' + pinyin[:4]
+
+    return '9999'
+9

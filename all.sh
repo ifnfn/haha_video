1e95e237ab7366d52791036c517020304b915a72
zhuzhg <zhuzhg@nationalchip.com>
2014-03-16 19:45:43 +0800

    update

--- all.sh
+++ all.sh
@@ -6,12 +6,26 @@ UpdateAlbum() {
     curl "$URL/manage/update?cmd=list&engine=$1Engine" && ./super_client.py
 }
 
-UpdateAlbum Livetv
-UpdateAlbum Qiyi
-UpdateAlbum QQ
-UpdateAlbum Sohu
-#UpdateAlbum PPtv
-#UpdateAlbum Letv
+UpdateScore() {
+    curl "$URL/manage/update?cmd=score&engine=$1Engine" && ./super_client.py
+}
 
-curl "$URL/manage/update?cmd=score" && ./super_client.py
 
+Update() {
+    UpdateAlbum Livetv
+    UpdateAlbum Qiyi
+    UpdateAlbum QQ
+    UpdateAlbum Sohu
+    #UpdateAlbum PPtv
+    #UpdateAlbum Letv
+}
+
+Score() {
+    #UpdateScore Livetv
+    UpdateScore Qiyi
+    UpdateScore QQ
+    UpdateScore Sohu
+}
+
+#Update
+Score

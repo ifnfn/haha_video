75b181dd4749ba7a148b3d77bb0b15af38b5326b
zhuzhg <zhuzhg@nationalchip.com>
2014-06-17 14:45:21 +0800

    修复宁波电视台

---
+++ scripts/nbtv.lua
@@ -0,0 +1,36 @@
+function get_video_url(url, id, referer)
+	print(url, id, referer)
+	local text = kola.wget(url, false)
+
+	if text and text ~= "TVie Exception: No streams." then
+		local d = os.date("*t", kola.gettime())
+		local data_obj = cjson.decode(text)
+		if data_obj then
+			return string.format('http://zb.nbtv.cn:8134/hls-live/livepkgr/_definst_/liveevent/%s_1.m3u8', data_obj.channel_name)
+		end
+	end
+
+	return ''
+end
+
+function get_channel(vid, id)
+	local url = string.format("%s/0/%d", vid, kola.gettime())
+
+	local ret = {}
+	text = kola.wget(url, false)
+	if text and text ~= "TVie Exception: No streams." then
+		local d = os.date("*t", kola.gettime())
+		local prev_d = d
+		local js = cjson.decode(text)
+		for k,v in ipairs(js.result[1]) do
+			ret[k] = {}
+			s = tonumber(v.start_time)
+			ret[k].time_string = os.date("%H:%M", s)
+			ret[k].time        = s
+			ret[k].duration    = tonumber(v.end_time) - s
+			ret[k].title       = v.name
+		end
+	end
+
+	return cjson.encode(ret)
+end

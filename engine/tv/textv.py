b72b21c28dc69e17423899e898216d6c72064ff2
zhuzhg <zhuzhg@nationalchip.com>
2014-07-09 18:33:50 +0800

    增加山西、大庆电视台

--- engine/tv/textv.py
+++ engine/tv/textv.py
@@ -15,6 +15,17 @@ class TextLivetvParser(LivetvParser):
         self.tvs = {
             '池州-新闻综合': 'rtmp://60.174.36.89:1935/live/vod4',
             '池州-经济频道': 'rtmp://60.174.36.89:1935/live/vod3',
+
+            '山西-经济资讯': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiFinance ',
+            '山西-黄河频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=HuangHeNews',
+            '山西-影视频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiFilm',
+            '山西-科教频道': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiEdu',
+            '山西-公共频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiPublic',
+            '山西-少儿频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiChild',
+            '大庆-影视频道': 'rtmp://live1.baihuwang.com:1935/live/ys',
+            '大庆-新闻综合': 'rtmp://live1.baihuwang.com:1935/live/zh',
+            '大庆-直播频道': 'rtmp://live1.baihuwang.com:1935/live/kj',
+            '大庆-百湖频道': 'rtmp://live1.baihuwang.com:1935/live/bh',
         }
 
         self.cmd['text'] = 'OK'

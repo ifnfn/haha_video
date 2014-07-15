7469db0da6e520e19837037402447b3ff5cd7bd3
zhuzhg <zhuzhg@nationalchip.com>
2014-07-15 20:48:31 +0800

    update tv order.

--- sdk/main.cpp
+++ sdk/main.cpp
@@ -767,7 +767,7 @@ int main(int argc, char **argv)
 //	test_picture1("电影"); return 0;
 //	test_custommenu(); return 0;
 //	printf("Test LiveTV(No EPG\n"); test_livetv_noepg(); return 0;
-	printf("Test LiveTV(TV List\n"); test_livetv_list(); return 0;
+//	printf("Test LiveTV(TV List\n"); test_livetv_list(); return 0;
 //	printf("Test LiveTV(TV List\n"); test_livetv_epglist(); return 0;
 	printf("Test LiveTV(TV List\n"); test_livetv(); return 0;
 

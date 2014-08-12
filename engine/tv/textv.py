575441f1a64c3a1f1d90177cc6c7c24df7f626cc
zhuzhg <zhuzhg@nationalchip.com>
2014-08-12 10:00:26 +0800

    fix tv alias

--- engine/tv/textv.py
+++ engine/tv/textv.py
@@ -13,19 +13,19 @@ class TextLivetvParser(LivetvParser):
 
         self.order = PRIOR_DEFTV
         self.tvs = {
-            '池州-新闻综合': 'rtmp://60.174.36.89:1935/live/vod4',
-            '池州-经济频道': 'rtmp://60.174.36.89:1935/live/vod3',
+            '池州-新闻': 'rtmp://60.174.36.89:1935/live/vod4',
+            '池州-经济': 'rtmp://60.174.36.89:1935/live/vod3',
 
-            '山西-经济资讯': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiFinance ',
-            '山西-黄河频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=HuangHeNews',
-            '山西-影视频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiFilm',
-            '山西-科教频道': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiEdu',
-            '山西-公共频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiPublic',
-            '山西-少儿频道': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiChild',
-            '大庆-影视频道': 'rtmp://live1.baihuwang.com:1935/live/ys',
-            '大庆-新闻综合': 'rtmp://live1.baihuwang.com:1935/live/zh',
-            '大庆-直播频道': 'rtmp://live1.baihuwang.com:1935/live/kj',
-            '大庆-百湖频道': 'rtmp://live1.baihuwang.com:1935/live/bh',
+            '山西-经济': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiFinance ',
+            '山西-黄河': 'http://218.26.188.211/flvss?bitrate=800000&channel=HuangHeNews',
+            '山西-影视': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiFilm',
+            '山西-科教': 'http://218.26.188.207/flvss?bitrate=800000&channel=Shan1XiEdu',
+            '山西-公共': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiPublic',
+            '山西-少儿': 'http://218.26.188.211/flvss?bitrate=800000&channel=Shan1XiChild',
+            '大庆-影视': 'rtmp://live1.baihuwang.com:1935/live/ys',
+            '大庆-新闻': 'rtmp://live1.baihuwang.com:1935/live/zh',
+            '大庆-直播': 'rtmp://live1.baihuwang.com:1935/live/kj',
+            '大庆-百湖': 'rtmp://live1.baihuwang.com:1935/live/bh',
         }
 
         self.cmd['text'] = 'OK'
